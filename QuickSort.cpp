24cefe8a3de4dad62af5749a7661bb1b21b0116d
Unknown <pyatkoa@gmail.com>
2018-01-09 18:55:45 +0300

    Not working

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -17,6 +17,51 @@ QuickSort::~QuickSort()
 {
 }
 
+bool mycomparefunction(double  a, double  b) { return ((a) < (b)); }
+
+static int getOpponent(int rank, int iteration) {
+
+    return rank ^ static_cast<int>(std::pow(2, iteration));
+
+}
+
+static double* mergeArrays(double* a, double* b, int sizeA, int sizeB) {
+    int resultSize = sizeA + sizeB;
+    double* result = new double[resultSize];
+    int indexA = 0;
+    int indexB = 0;
+
+    for (int i = 0; i < resultSize; i++) {
+
+        if (indexA == sizeA) {
+            result[i] = b[indexB];
+            indexB += 1;
+        }
+        else if (indexB == sizeB) {
+            result[i] = a[indexA];
+            indexA += 1;
+        }
+        else if (a[indexA] < b[indexB]) {
+            result[i] = a[indexA];
+            indexA += 1;
+        }
+        else {
+            result[i] = b[indexB];
+            indexB += 1;
+        }
+    }
+    return result;
+}
+
+static int intPower(int a, int b) {
+    int result = 1;
+    for (int i = 0; i<b; i++) {
+        result *= a;
+    }
+    return result;
+}
+
+
 double* QuickSort::run(const Vector& vec)
 {
     // Инициализация MPI
@@ -47,16 +92,28 @@ double* QuickSort::run(const Vector& vec)
     int* packChunkSize = new int[1];
     double* input = vec.m_Vector;//new double[sizeInput] { 0, 1, 2, 5, 7, 3, 4, 7, 3, 1, 5, 7, 3, 46, 67, 678 };
 
-    if (rank == 0)
-    {
+    if (rank == 0) {
+        //            try {
+        //                input = Files
+        //                        .lines(Paths.get("numbers.txt"), StandardCharsets.UTF_8)
+        //                        .mapToDouble(Double::valueOf)
+        //                        .toArray();
+        //            } catch (IOException e) {
+        //                e.printStackTrace();
+        //            }
+
+        //            input = ListGenerator.generate(64);
+
         int N = sizeInput;
         packChunkSize[0] = N / threadCount;
+
     }
 
     clock_t begin = clock();
 
     // Рассылаем куски процессам
 
+    //MPI_Bcast() sends the same piece of data to everyone, while MPI_Scatter() sends each process a part of the input array. MPI_Bcast() is the opposite of MPI_Reduce() and MPI_Scatter() is the opposite of MPI_Gather().
     MPI_Bcast(packChunkSize, 1, MPI_INT, 0, MPI_COMM_WORLD); //Передача от одного процесса всем процессам (широковещательная рассылка)
 
 
@@ -73,6 +130,7 @@ double* QuickSort::run(const Vector& vec)
 
     // Локально сортируем, определяем опорный элемент
     std::sort(local, local + localSize, mycomparefunction);
+    //Arrays.sort(local);
 
     double* packCoreElement = new double[1];
 
@@ -80,6 +138,7 @@ double* QuickSort::run(const Vector& vec)
         packCoreElement[0] = local[0];
     }
 
+    //comm.Bcast(packCoreElement, 0, 1, MPI.DOUBLE, 0);
     MPI_Bcast(packCoreElement, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD); //Передача от одного процесса всем процессам (широковещательная рассылка)
     double core = packCoreElement[0];
 
@@ -87,20 +146,27 @@ double* QuickSort::run(const Vector& vec)
 
     int iterCount = (int)(log(threadCount) / log(2));
 
-    for (int iter = 0; iter < iterCount; iter++)
-    {
+    for (int iter = 0; iter < iterCount; iter++) {
+        //int coreIndex = Arrays.binarySearch(local, core);
         int coreIndex = std::binary_search(local, local + localSize, core);
-        if (coreIndex < 0)
-        {
+        if (coreIndex < 0) {
             coreIndex = coreIndex * (-1) - 1;
         }
         int opponent = getOpponent(rank, iter);
 
 
-        if (rank < opponent)
-        {
+        if (rank < opponent) {
+            //comm.Isend(local, coreIndex, local.length - coreIndex, MPI.DOUBLE, opponent, rank);
+            //(Object buf, int offset, int count, Datatype datatype, int dest, int tag);
+
+
             MPI_Isend(local + coreIndex, localSize - coreIndex, MPI_DOUBLE, opponent, rank, MPI_COMM_WORLD, &request);
+
+            //local = Arrays.copyOfRange(local, 0, coreIndex);
             memcpy(local, local, coreIndex);
+
+            //Status status = comm.Probe(opponent, opponent);
+            //Probe(int source, int tag)
 
             MPI_Status status1;
             MPI_Probe(opponent, opponent, MPI_COMM_WORLD, &status1);
@@ -108,23 +174,30 @@ double* QuickSort::run(const Vector& vec)
             MPI_Get_count(&status1, MPI_DOUBLE, &count);
 
             double* newarr = new double[count];
+            //comm.Recv(newarr, 0, status.count, MPI.DOUBLE, opponent, opponent);
+            //Recv(Object buf, int offset, int count, Datatype datatype, int source, int tag);
 
             MPI_Recv(newarr, count, MPI_DOUBLE, opponent, opponent, MPI_COMM_WORLD, &status1);
             local = mergeArrays(local, newarr, sizeInput, count);
             localSize = sizeInput + count;
 
         }
-        else
-        {
+        else {
+
+            //comm.Isend(local, 0, coreIndex, MPI.DOUBLE, opponent, rank);
+            //comm.Isend(Object buf, int offset, int count, Datatype datatype, int dest, int tag)
+
             MPI_Isend(local, coreIndex, MPI_DOUBLE, opponent, rank, MPI_COMM_WORLD, &request);
 
             memcpy(local, local + coreIndex, localSize);
+            //local = Arrays.copyOfRange(local, coreIndex, local.length);
 
             MPI_Status status1;
             MPI_Probe(opponent, opponent, MPI_COMM_WORLD, &status1);
             int count;
             MPI_Get_count(&status1, MPI_DOUBLE, &count);
             double* newarr = new double[count];
+            //comm.Recv(newarr, 0, count, MPI.DOUBLE, opponent, opponent);
             MPI_Recv(newarr, count, MPI_DOUBLE, opponent, opponent, MPI_COMM_WORLD, &status1);
             local = mergeArrays(local, newarr, sizeInput, count);
             localSize = sizeInput + count;
@@ -136,6 +209,9 @@ double* QuickSort::run(const Vector& vec)
 
     int* counts = new int[threadCount];
     int* localCount = new int [1] { localSize };
+    //comm.Gather(localCount, 0, 1, MPI.INT, counts, 0, 1, MPI.INT, 0);
+    //Gather(Object sendbuf, int sendoffset, int sendcount, Datatype sendtype, Object recvbuf, int recvoffset, int recvcount, Datatype recvtype, int root)
+
     MPI_Gather(localCount, 1, MPI_INT, counts, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
     int* offsets = new int[threadCount];
@@ -148,8 +224,13 @@ double* QuickSort::run(const Vector& vec)
 
     // Собираем куски
 
+    //comm.Gatherv(local, 0, local.length, MPI.DOUBLE, input, 0, counts, offsets, MPI.DOUBLE, 0);
+    //Gatherv(Object sendbuf, int sendoffset, int sendcount, Datatype sendtype, Object recvbuf, int recvoffset, int[] recvcount, int[] displs, Datatype recvtype, int root)
+
+
     MPI_Gatherv(local, localSize, MPI_DOUBLE, input, counts, offsets, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
+    //long stopTime = System.currentTimeMillis();
     clock_t end = clock();
 
     if (rank == 0) {
@@ -160,8 +241,12 @@ double* QuickSort::run(const Vector& vec)
 
 //    MPI_Wait(&request, &status);
 
+    //comm.Barrier();
     MPI_Barrier(MPI_COMM_WORLD);
+    //MPI.Finalize();
     MPI_Finalize();
+    system("pause");
+
     return input;
 }
 
--- main.cpp
+++ main.cpp
@@ -1,21 +1,136 @@
-#include <QCoreApplication>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <time.h>
+
+#include "CSVManager.h"
 #include "Vector.h"
 #include "QuickSort.h"
+
 using namespace std;
 
-int main(int argc, char *argv[])
+void myQuickSort(double* arr, int left, int right) {
+      int i = left, j = right;
+      int tmp;
+      int pivot = arr[(left + right) / 2];
+
+      /* partition */
+
+      while (i <= j)
+      {
+            while (arr[i] < pivot)
+                  i++;
+            while (arr[j] > pivot)
+                  j--;
+            if (i <= j)
+            {
+                  tmp = arr[i];
+                  arr[i] = arr[j];
+                  arr[j] = tmp;
+                  i++;
+                  j--;
+            }
+      };
+
+      /* recursion */
+      if (left < j)
+            myQuickSort(arr, left, j);
+      if (i < right)
+            myQuickSort(arr, i, right);
+}
+
+int main(int argc, char** argv)
 {
-    QCoreApplication a(argc, argv);
+    if (argc >= 3)
+    {
+        string command = string(argv[1]);
 
-    Vector vec;
-    vec = Vector(1000, 10000, 1);
-    cout << "VECTOR : ";
-    cout << vec << endl;
+        if (command == "create")
+        {
+            string arg1(argv[2]);
+            string arg2(argv[3]);
 
-    Vector out(vec.m_Size);
-    QuickSort sort;
-    out.m_Vector = sort.run(vec);
-    cout << "FINAL: " << out;
+            Vector tmp(stoi(arg1), 1000, 10);
+            string nameOfFile = arg2;
+            ofstream file(nameOfFile);
+            file << tmp;
+            cout << "Vector " << nameOfFile << " created!" << endl;
+        }
+        if (command == "sort")
+        {
+            string type(argv[2]);
+            ifstream file1(argv[3]);
+            string nameForResultMatrix(argv[4]);
+            string nameOfOutputFile(argv[5]);
 
-    return a.exec();
+            Vector vec;
+
+            if(file1)
+            {
+                file1 >> vec;
+            }
+            else
+            {
+                cout << "NO THIS FILE WITH VECTOR" << endl;
+                system ("pause");
+                return -1;
+            }
+
+            Vector resultVector(vec.m_Size);
+
+            double start_time = 0;
+            double end_time = 0;
+            double search_time = 0;
+
+            if (type == "std")
+            {
+                Vector tmpVec = vec;
+                for (int i = 0; i < 10; i++)
+                {
+                    start_time = clock();
+
+                    myQuickSort(vec.m_Vector, 0, vec.m_Size - 1);
+
+                    end_time = clock();
+                    search_time += (end_time - start_time)/1000.0f;
+                }
+                resultVector = vec;
+            }
+            if (type == "mpi")
+            {
+                start_time = clock();
+
+                QuickSort sort;
+                resultVector.m_Vector = sort.run(vec);
+
+                end_time = clock();
+                search_time += (end_time - start_time)/1000.0f;
+            }
+
+            nameOfOutputFile += ".csv";
+            fstream file(nameOfOutputFile, ios::in);
+            CSVManager CSV;
+
+            file >> CSV;
+            file.close();
+            file.open(nameOfOutputFile, ios::out);
+
+            CSV.addInformation(resultVector.m_Size, 1, type, search_time);
+            file << CSV;
+            file.close();
+
+            ofstream fileoutput(nameForResultMatrix);
+            fileoutput << resultVector;
+        }
+        else
+        {
+            cout << "No this command:" << argv[1] << endl;
+        }
+    }
+
+    cout << "END" << endl;
+    return 0;
 }
